1b40ee3ddd2a4e42425c2bcff4deead89fdcf486
user.email <gingrasgianat9xtxm@hotmail.com>
2024-10-29 04:56:46 -0700

    add online page basic ui

--- src/pages/core/onlinePlayer/online.jsx
+++ src/pages/core/onlinePlayer/online.jsx
@@ -2,18 +2,50 @@ import React, { useEffect } from "react";
 import { Header, Layout, Menu } from "../../../common/components";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import moment from "moment";
+import { getOnlinePlayers } from "../../../api/user";
 
 export const OnlinePlayers = () => {
-  const user = useSelector(({ user }) => user.user);
-  const round = useSelector(({ round }) => round.info);
+  const onlinePlayers = useSelector(({ online }) => online.onlinePlayers);
 
   const dispatch = useDispatch();
-
-
+  useEffect(() => {
+    getOnlinePlayers(dispatch)
+  },[])
   return (
     <Layout currentActiveTab={"headquarters"}>
-     <div>adfadfadfadf</div>
+      <div className="flex-1 mx-5">
+        <div className="text-[#FFFF00] text-[24px] font-[1000] my-5 underline">
+          {onlinePlayers.length} Online Players
+        </div>
+        <div className="border-[#FFFF00] border-[1px] w-full min-h-[500px] h-auto text-[#FFFF00] ">
+          <div className="w-full flex text-center font-bold">
+            <div className="w-[10%] border-[#FFFF00] border-[1px] leading-7">No</div>
+            <div className="w-[30%] border-[#FFFF00] border-[1px] leading-7">Name</div>
+            <div className="w-[30%] border-[#FFFF00] border-[1px] leading-7">
+              Character Type
+            </div>
+            <div className="w-[30%] border-[#FFFF00] border-[1px] leading-7">Level</div>
+          </div>
+          {onlinePlayers.length == 0 ? (
+            <div className="text-center mt-5">No Online Players</div>
+          ) : (
+            onlinePlayers.map((user, index) => (
+              <div className="w-full flex text-center" key={index}>
+                <div className="w-[10%] border-[#FFFF00] border-[1px] leading-7">{index+1}</div>
+                <a className="w-[30%] border-[#FFFF00] border-[1px] leading-7 underline" href="#">
+                  {user.name || "--"}
+                </a>
+                <div className="w-[30%] border-[#FFFF00] border-[1px] leading-7">
+                  {user.characterType || "--"}
+                </div>
+                <div className="w-[30%] border-[#FFFF00] border-[1px] leading-7">
+                  {user.level || "--"}
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
     </Layout>
   );
 };
