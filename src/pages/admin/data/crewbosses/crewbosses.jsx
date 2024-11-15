feeed202dc8f97de6b5027b58104db7505bdba01
486xtm <gingrasgianat9xtxm@hotmail.com>
2024-11-15 20:08:29 +0800

    nuke country admin page

---
+++ src/pages/admin/data/crewbosses/crewbosses.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CrewBosses = () => {
+  return <div>
+    Crew boss
+  </div>
+}
---
+++ src/pages/admin/data/shop/shop.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ShopItems = () => {
+  return <div>
+    shop
+  </div>
+}
