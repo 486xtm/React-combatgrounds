99bdf6ee0fb1aa73e99bb6a5ae095fea563c9702
486xtm <gingrasgianat9xtxm@hotmail.com>
2024-11-17 20:33:54 +0800

    add hightest crew netWorth on hof page

--- src/pages/core/hof/hof.jsx
+++ src/pages/core/hof/hof.jsx
@@ -23,6 +23,8 @@ export const HallOfFame = () => {
     <Layout currentActiveTab={"hall-of-fame"}>
       <div className="flex flex-col flex-1">
         <img src="/pics/halloffame.gif" alt="halloffame.gif" className="mx-3" />
+        {hofData && !(Number(roundId || hofData.currentRound - 1) !==
+              hofData.currentRound - 1) && <>
         <table className={styles["custom-table"]}>
           <tbody>
             <tr>
@@ -56,14 +58,30 @@ export const HallOfFame = () => {
               <td>Net Worth</td>
             </tr>
             <tr>
-              <td>007</td>
-              <td>71,787,670</td>
+              <td>Black Rose</td>
+              <td>117,688,630</td>
             </tr>
           </tbody>
         </table>
 
+        <table className={styles["custom-table"]}>
+          <tbody>
+            <tr>
+              <td colSpan={2}>All-Time Highest Crew Networth</td>
+            </tr>
+            <tr>
+              <td>Name</td>
+              <td>Net Worth</td>
+            </tr>
+            <tr>
+              <td>Swamp Ass</td>
+              <td>367,529,832</td>
+            </tr>
+          </tbody>
+        </table>
+        </>}
         <p className="text-secondary text-xl text-center font-bold mt-5">
-          Results for Round{" "}
+        Results for Round{" "}
           {hofData && roundId
             ? roundId
             : hofData
@@ -94,7 +112,7 @@ export const HallOfFame = () => {
                 <tr key={`top_supporter_${idx}`}>
                   <td>{idx + 1}</td>
                   <td>{l.player.name}</td>
-                  <td>{l.netWorth}</td>
+                  <td>{Number(l.netWorth).toLocaleString()}</td>
                   <td>{l.medals.name}</td>
                 </tr>
               ))}
@@ -119,7 +137,7 @@ export const HallOfFame = () => {
                 <tr key={`top_free_${idx}`}>
                   <td>{idx + 1}</td>
                   <td>{l.player.name}</td>
-                  <td>{l.netWorth}</td>
+                  <td>{Number(l.netWorth).toLocaleString()}</td>
                   <td>{l.medals.name}</td>
                   <td>{l.prize}</td>
                 </tr>
@@ -147,7 +165,7 @@ export const HallOfFame = () => {
                   <td>{idx + 1}</td>
                   <td>{l.name}</td>
                   <td>{l.leader.name}</td>
-                  <td>{l.netWorth}</td>
+                  <td>{Math.floor(Number(l.netWorth)).toLocaleString()}</td>
                   <td>{l.medals.name}</td>
                   <td>{l.prize}</td>
                 </tr>
@@ -157,7 +175,7 @@ export const HallOfFame = () => {
         <div className="flex w-full justify-center gap-20 my-5 items-center">
           <p className="text-sm text-secondary">Go to Round:</p>
           <input
-            className="w-[70px] rounded-lg -ml-[65px] px-2"
+            className="w-[70px] rounded-lg -ml-[65px] px-2 text-center"
             onChange={(ev) => setGoRound(ev.target.value)}
           />
           <button
