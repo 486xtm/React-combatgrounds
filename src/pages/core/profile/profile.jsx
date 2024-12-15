ed101bd3de335edd595474c5c4e52bb6f6902956
486xtm <gingrasgianat9xtxm@hotmail.com>
2024-12-16 02:25:51 +0800

    fix profile avatar

--- src/pages/core/profile/profile.jsx
+++ src/pages/core/profile/profile.jsx
@@ -406,7 +406,7 @@ export const Profile = () => {
                 </div>
               ) : null}
             </div>
-            <div className="flex flex-col px-3 py-2 flex-1">
+            <div className="flex flex-col px-3 py-2 flex-1 w-1/2">
               <div
                 className={`my-1 ${
                   user.characterType === "Soldier"
@@ -420,7 +420,7 @@ export const Profile = () => {
               >
                 DESCRIPTION
               </div>
-              <div className="text-xs text-white hyphens-auto">
+              <div className="text-xs text-white hyphens-auto break-all">
                 {user && user.description}
               </div>
               <div
